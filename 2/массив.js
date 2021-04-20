177ce39ece3b3526622a7b7577977990e0ccab37
penivaiz <1.4.hoffer@gmail.com>
2021-04-20 19:10:18 +0300

    первый блок

---
+++ 2/массив.js
@@ -0,0 +1,11 @@
+var car = ['Ford', 'Mazda', 'Kia']
+var any = [42, 'Apple', {a: 1}] //массив принимает любые типы данных
+
+
+console.log(car[1]) //Mazda
+console.log(car.length) // длина массива 
+car.push('bmw') // добавление в конец массива
+car.pop() //удаление последнего элемента массива 
+
+car.shift() //удаление первого элемента
+car.unshift() //добавление в начало 
